192246a48aec2993c3587caeee31e717b2315c85
Albert <albertoma24@gmail.com>
2025-03-24 19:12:50 -0700

    Mongoose lab Monday

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const prompt = require('prompt-sync')();
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
 
 const username = prompt('What is your name? ');
-
 console.log(`Your name is ${username}`);
